5d6e65e080c39563a89303924b72d64476b34536
Gustavo Castilião <gucastiliao@gmail.com>
2018-01-05 22:05:46 -0200

    chore(database): move db infos to .env file

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-module.exports = mongoose.connect('mongodb://localhost/messenger-bot')
+module.exports = mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`)
   .then(() => console.warn('Database connection succesful'))
   .catch(error => console.error(error));
